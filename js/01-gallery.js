f30a6b3746214a3dd0788236dd3ec18135266447
Maksym150678 <civicm110@gmail.com>
2022-07-08 17:17:13 +0300

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+const galleryList = document.querySelector('.gallery');
+const galleryCardsArr = galleryItems
+.map(
+    (el) => 
+    `<div class="gallery__item">
+    <a class="gallery__link" href="${el.original}">
+      <img
+        class="gallery__image"
+        src="${el.preview}"
+        data-source="${el.original}"
+        alt="${el.description}"
+      />
+    </a>
+  </div>`
+)
+.join("");
 
-console.log(galleryItems);
+
+galleryList.innerHTML = galleryCardsArr;
+galleryList.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const target = event.target;
+    if (target.nodeName !== "IMG") {
+        return;
+    }
+
+    const instance = basicLightbox.create(
+
+        `<img src="${target.dataset.source}" width="800" height="600">`
+    );
+    instance.show();
+    
+    window.addEventListener('keydown', onEscBtnPush);
+
+    function onEscBtnPush (event) {
+        if (event.code !== 'Escape') {
+            return;
+            }
+            instance.close();
+        
+    }
+});
+
