9df96a49b00c3d482cf094a3476351f9a2f6760c
Maksym150678 <civicm110@gmail.com>
2022-07-10 16:19:20 +0300

    1620 100722 task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,52 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+const galleryList = document.querySelector('.gallery');
+const galleryCardsArr = galleryItems
+.map(
+    (el) => 
+    `<li class="gallery__link">
+    <a class="gallery__link" href="${el.original}">
+      <img
+        class="gallery__image"
+        src="${el.preview}"
+        data-source="${el.original}"
+        alt="${el.description}"
+      />
+    </a>
+  </li>`
+)
+.join("");
 
-console.log(galleryItems);
+
+galleryList.innerHTML = galleryCardsArr;
+galleryList.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const target = event.target;
+    if (target.nodeName !== "IMG") {
+        return;
+    }
+
+    const instance = basicLightbox.create(
+
+        `<img src="${target.dataset.source}" width="800" height="600">`
+    );
+    instance.show();
+    
+    window.addEventListener('keydown', onEscBtnPush);
+
+    function onEscBtnPush (event) {
+        if (event.code !== 'Escape') {
+            return;
+            }
+            instance.close();
+        
+    }
+});
+
+galleryList.insertAdjacentHTML("afterbegin", galleryCardsArr.join(""));
+
+var lightbox = new SimpleLightbox(".gallery a", {
+captionsData: "alt",
+captionPosition: "bottom",
+captionDelay: 250,
+});
